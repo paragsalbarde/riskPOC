1408f723ff644abc7334ec053246d491e792b1d6
bindokparaag <parag.salbarde@gmail.com>
2020-05-12 09:52:04 +0530

    chart width and color palltes

--- src/app/d3-charts/donutchart/donutchart.component.ts
+++ src/app/d3-charts/donutchart/donutchart.component.ts
@@ -20,13 +20,14 @@ export class DonutchartComponent implements OnInit {
 
   ngOnInit() {
     var self = this;  
-    var width = 330;
-    var height = 330;
+    var width = 275;
+    var height = 275;
     var radius = Math.min(width, height) / 2;
     var arrRisksColor = ['External', 'Internal', 'Low', 'Medium','High', 'Critical', 'No Risk'];
     var color = d3.scaleOrdinal()
                 .domain(arrRisksColor)
                 //.range(["#49d9eb", "#00a5b6", "#95d7ff" , "#7bbfff", "#016da9", "#62a8e9", "#dedede"]);
+                .range(["#49d9eb", "#00a5b6", "#95d7ff" , "#7bbfff", "#016da9", "#ef6b71", "#dedede"]);
                 //.range(['#da8f34', '#142459', '#eabd3b', '#ee9a3a', '#de542c', '#820401', '#f7f4bf']);
                 //.range(["#49d9eb", "#00a5b6", "#A8DADC" , "#457B9D", "#1D3557", "#E63946", "#F1FAEE"]);
                 //.range(["#227c9d", "#17c3b2", "#84A98C" , "#52796F", "#354F52", "#E63946", "#CAD2C5"]);
@@ -35,9 +36,12 @@ export class DonutchartComponent implements OnInit {
                 //.range(["#063E54", "#079E8C", "#daffef", "#c0fdfb", "#64b6ac", "#5d737e", "#fcfffd"]);
                 //.range(["#063E54", "#079E8C", "#c0fdfb", "#64b6ac", "#5d737e", "#EF6B71", "#fcfffd"]);
                 //.range(["#063E54", "#079E8C", "#02c39a", "#028090","#05668d", "#EF6B71", "#fcfffd"]);
-                 .range(["#063E54", "#079E8C", "#DBE6DE", "#B7C5C2","#8EA1A2", "#E63946", "#F3F3F3"]);
+                 //.range(["#063E54", "#079E8C", "#DBE6DE", "#B7C5C2","#8EA1A2", "#E63946", "#F3F3F3"]);
+                 //.range(["#247C9E", "#079E8C", "#DBE6DE", "#B7C5C2","#8EA1A2", "#E63946", "#F3F3F3"]);
+                 //.range(["#247C9E", "#079E8C", "#fee89a", "#fdbf70","#f78851", "#e35449", "#f7faaf"]);
+
                 
-                
+                 
 
     var svg = d3.select('#'+this.chartID)
                 .append('svg')
